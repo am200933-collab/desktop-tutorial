9b26ba27353e2ddd00994a1aeddd9769411c13a9
am200933-collab <am200933@gmail.com>
2026-06-24 10:57:18 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'nativeworkout-pwa-v1';
+const CACHE = 'nativeworkout-pwa-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))));
